d8fee0276089294e2d3c04ed5b444ff792746af9
Max Melcher <Max.Melcher@microsoft.com>
2019-05-25 22:34:50 +0200

    added xunit test

---
+++ AzureDevOps.WikiPDFExport.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AzureDevOps.WikiPDFExport.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
