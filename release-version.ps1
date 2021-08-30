1f21d6f3075305ab37103a130f2f2b2c9bd72cf0
Max Melcher <max@melcher.it>
2021-08-31 01:21:44 +0200

    improved the version script

--- release-version.ps1
+++ release-version.ps1
@@ -1,4 +1,6 @@
 # dotnet tool install --global dotnet-warp
 
-$version = "4.0.0-beta"
+$version = "4.0.0-beta2"
 dotnet warp AzureDevOps.WikiPDFExport/azuredevops-export-wiki.csproj -p:Version=$version
+git tag -a "v$version" -m "v$version"
+git push origin "v$version"
