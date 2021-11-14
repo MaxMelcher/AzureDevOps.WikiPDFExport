5fc3a68d51e21a9c69f7be752f989d9bdb2dbbdf
Max Melcher <max@melcher.it>
2021-11-15 00:50:41 +0100

    Actions

--- release-version.ps1
+++ release-version.ps1
@@ -1,6 +1,6 @@
 # handled in github actions
 $version = "4.0.0-beta5"
-dotnet publish -r win-x64 -c Release -p:Version=$version -o output/win-x64
+dotnet publish -r win-x64 -c Release -p:Version=$version -o output/win-x64 --self-contained
 
 #linux version
-dotnet publish -r linux-x64 --configuration Release -p:PublishReadyToRun=false -p:PublishSingleFile=true  -p:Version=$version -o output/linux-x64
+dotnet publish -r linux-x64 --configuration Release -p:PublishReadyToRun=false -p:PublishSingleFile=true  -p:Version=$version -o output/linux-x64 --no-self-contained
