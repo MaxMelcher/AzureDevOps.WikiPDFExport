4f530f4a08ddd01e08b9b2f32f70d32ed3590795
gcerik <82101285+gcerik@users.noreply.github.com>
2022-08-03 11:48:54 -0400

    added hierarchical export for single file

--- AzureDevOps.WikiPDFExport/WikiScanners/SingleFileScanner.cs
+++ AzureDevOps.WikiPDFExport/WikiScanners/SingleFileScanner.cs
@@ -1,37 +1,51 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.TeamFoundation.TestManagement.WebApi;
+using Microsoft.TeamFoundation.Wiki.WebApi;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace azuredevops_export_wiki
 {
-    internal class SingleFileScanner : IWikiDirectoryScanner
+    internal class SingleFileScanner : WikiOptionFilesScanner
     {
         private readonly string singleFilePath;
-        private ILogger logger;
 
-        public SingleFileScanner(string filePath, ILogger logger)
+        public SingleFileScanner(string filePath, string wikiPath, Options options, ILogger logger)
+            : base(wikiPath, options, logger) 
         {
             this.singleFilePath = filePath;
-            this.logger = logger;
         }
 
-        public IList<MarkdownFile> Scan()
+        public override IList<MarkdownFile> Scan()
         {
-            var filePath = Path.GetFullPath(singleFilePath);
-            var directory = new DirectoryInfo(Path.GetFullPath(filePath));
+            var allFiles = base.Scan(); 
+            var current = allFiles.FirstOrDefault(m => m.FileRelativePath.Contains(singleFilePath));
+            var idx = allFiles.IndexOf(current);
 
-            if (!File.Exists(filePath))
+            if (current is null || idx == -1)
             {
-                logger.Log($"Single-File [-s] {filePath} specified not found" + filePath, LogLevel.Error);
+                logger.Log($"Single-File [-s] {singleFilePath} specified not found" + singleFilePath, LogLevel.Error);
                 throw new ArgumentException($"{singleFilePath} not found");
             }
+            var result = new List<MarkdownFile>() { current };
 
-            var relativePath = filePath.Substring(directory.FullName.Length);
+            if (allFiles.Count > idx)
+            {
+                foreach (var item in allFiles.Skip(idx + 1))
+                {
+                    if (item.Level > current.Level)
+                    {
+                        result.Add(item);
+                    }
+                    else
+                        break;
+                }
+            }
 
-            return new List<MarkdownFile>() {
-                            new MarkdownFile(new FileInfo(filePath), relativePath, 0, "/")
-                        };
+            return result;
         }
 
     }
